46792ca1901256e08fe22b4279a2947f57f8c635
Juwon <ju1e3718@gmail.com>
2023-05-13 22:26:55 +0900

    add length limit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,7 @@ function Movie({ id, medium_cover_image, title, summary, genres }) {
         <div>
             <img src={medium_cover_image} alt="title" />
             <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
-            <p>{summary}</p>
+            <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
             <ul>
                 {genres.map(g => <li key={g}>{g}</li>)}
             </ul>
